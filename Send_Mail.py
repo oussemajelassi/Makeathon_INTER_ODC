a3e69a29ee0189a6312dcf3e573b8e98fd1b87b7
Oussama Jlassi <100140668+oussemajelassi@users.noreply.github.com>
2022-12-04 10:39:59 +0100

    Add files via upload

---
+++ Send_Mail.py
@@ -0,0 +1,18 @@
+import smtplib
+
+
+smtpObj = smtplib.SMTP('smtp.gmail.com', 587) # or 465 ken 587 ma7abech
+smtpObj.ehlo()
+smtpObj.starttls()
+smtpObj.login('ahmedmarnissi1905@gmail.com', 'bvzozluxjonwsnwr') # mail li bch tab3ath bih
+
+subject='mail'
+body='3asba'
+msg=f'subject:{subject}\n\n{body}'
+
+smtpObj.sendmail( 'ahmedmarnissi1905@gmail.com', 'gazze7achraf@gmail.com', msg)   
+#                    li tab2ath bih               mail recepteur      #contenu
+
+
+
+print ("Successfully sent email")
